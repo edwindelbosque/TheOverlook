d2dfd989054389b79770eb987178080846bb1787
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-09-11 13:46:09 -0600

    Add ability to fetch the API

--- src/index.js
+++ src/index.js
@@ -11,4 +11,28 @@ import './images/favicon.png';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let customers, rooms, bookings, roomServices;
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+  .then(response => response.json())
+  .then(data => customers = data.users)
+  .then(() => console.log('customers', customers))
+  .catch(err => console.log('Unable to fetch the data', err));
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  .then(response => response.json())
+  .then(data => rooms = data.rooms)
+  .then(() => console.log('rooms', rooms))
+  .catch(err => console.log('Unable to fetch the data', err));
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  .then(response => response.json())
+  .then(data => bookings = data.bookings)
+  .then(() => console.log('bookings', bookings))
+  .catch(err => console.log('Unable to fetch the data', err));
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
+  .then(response => response.json())
+  .then(data => roomServices = data.roomServices)
+  .then(() => console.log('roomServices', roomServices))
+  .catch(err => console.log('Unable to fetch the data', err));
