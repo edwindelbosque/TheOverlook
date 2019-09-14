643fef44e37fbffae94a2266a863cf4fcd37339e
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-09-14 16:40:33 -0600

    Pass spy tests for RoomService class file

--- test/RoomService-test.js
+++ test/RoomService-test.js
@@ -14,7 +14,8 @@ beforeEach(() => {
     'displayResetOrders',
     'displaySearchedOrders',
     'dusplayResetOrders',
-    'displayResultsHeader'
+    'displayResultsHeader',
+    'displayResetResults'
   ], () => true);
   roomService = new RoomService(roomServiceData);
 });
@@ -46,13 +47,20 @@ describe('RoomService', () => {
     roomService.getDailyServices('2019/10/27');
     expect(DOMupdates.displayResetOrders).to.have.been.called(1);
     expect(DOMupdates.displayOrdersToday).to.have.been.called(2);
-  })
+  });
 
   it('should return room services for a specific date', () => {
     expect(roomService.getDailyServices('2019/10/27')).to.deep.equal([
       "Generic Cotton Sandwich",
       "Unbranded Plastic Sandwich"
     ])
+  });
+
+  it('should call DOMupdates when searching for orders', () => {
+    roomService.searchOrders('2019/08/08');
+    expect(DOMupdates.displayResetResults).to.have.been.called(1);
+    expect(DOMupdates.displayResultsHeader).to.have.been.called(1);
+    expect(DOMupdates.displaySearchedOrders).to.have.been.called(3);
   });
 
 });
